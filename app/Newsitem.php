f1141f72fb44b1341ea413656ec2b260cd404e7e
TheSas99 <thesas99games@gmail.com>
2020-10-05 09:43:14 +0200

    Added Authentication

--- app/Newsitem.php
+++ app/Newsitem.php
@@ -2,80 +2,23 @@
 
 namespace App;
 
-class NewsItem
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\NewsItem
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|NewsItem newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NewsItem newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NewsItem query()
+ * @mixin \Eloquent
+ * @property-read \App\Category $category
+ */
+class NewsItem extends Model
 {
-    public $id;
-    public $title;
-    public $description;
-    public $image = null;
-    private $errors = [];
+    public $fillable = ['title', 'description', 'image', 'category_id'];
 
-    const ITEMS = [
-        [
-            "id" => 1,
-            "title" => "Vandaag is er iets gebeurd",
-            "description" => "Neuter cursus tandem promissios nix est. When drying puréed margerines, be sure they are room temperature. Sonic shower, pattern, and advice.",
-            "image" => "http://lorempixel.com/400/200/business/"
-        ],
-        [
-            "id" => 2,
-            "title" => "Iets met techniek",
-            "description" => "Neuter cursus tandem promissios nix est. When drying puréed margerines, be sure they are room temperature. All occult visitors meet each other, only celestine saints have a courage.",
-            "image" => "http://lorempixel.com/400/200/technics/"
-        ],
-        [
-            "id" => 3,
-            "title" => "Sporten is belangrijk",
-            "description" => "Neuter cursus tandem promissios nix est. When drying puréed margerines, be sure they are room temperature. The mighty bung hole calmly robs the wave.",
-            "image" => "http://lorempixel.com/400/200/sports/"
-        ]
-    ];
-
-    /**
-     * @return \string[][]
-     */
-    static public function all()
+    public function category()
     {
-        return self::ITEMS;
-    }
-
-    /**
-     * @param int $id
-     * @return string[]
-     * @throws \Exception
-     */
-    static public function find(int $id)
-    {
-        $itemIndex = array_search($id, array_column(self::ITEMS, 'id'));
-
-        if ($itemIndex === false) {
-            throw new \Exception("ID does not exist in NewsItem");
-        }
-
-        return self::ITEMS[$itemIndex];
-    }
-
-    /**
-     * @return bool
-     */
-    public function save()
-    {
-        if (empty($this->title)) {
-            $this->errors['title'] = 'Titel moet ingevuld zijn';
-        }
-        if (empty($this->description)) {
-            $this->errors['description'] = 'Beschrijving moet ingevuld zijn';
-        }
-
-        //True/false based on state of errors
-        return empty($this->errors);
-    }
-
-    /**
-     * @return array
-     */
-    public function getErrors(): array
-    {
-        return $this->errors;
+        return $this->belongsTo(Category::class);
     }
 }
