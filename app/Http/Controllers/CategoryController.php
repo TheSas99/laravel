21f5b6d12de1a4d063be28b41f917cf6545fa331
TheSas99 <thesas99games@gmail.com>
2020-10-22 13:09:16 +0200

    Added comment section

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,6 +10,6 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Category::with('newsItems')->get();
-     return view('categories.index', compact('categories'));
+        return view('categories.index', compact('categories'));
     }
 }
--- app/Http/Controllers/NewsItemController.php
+++ app/Http/Controllers/NewsItemController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class NewsItemController extends Controller
 {
+
     public function index()
     {
         $newsItems = NewsItem::all();
